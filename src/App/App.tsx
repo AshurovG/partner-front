504179c4f052ae7657d08d2eb24ae2305f1beef7
Ashurov Georgy <ashurov1308@mail.ru>
2024-03-04 17:48:30 +0300

    add checking auth method

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { ToastContainer } from 'react-toastify';
+import { useDispatch } from "react-redux";
+import { useIsAuth, setIsAuthAction } from "slices/AuthSlice"
 import 'react-toastify/dist/ReactToastify.css';
 import MainPage from "pages/MainPage"
 import AshurovCardPage from "pages/AshurovCardPage"
@@ -15,26 +19,61 @@ import AboutCompanyBlock from "components/AboutCompanyBlock"
 import CategoryPage from "pages/CategoryPage"
 
 const App = () => {
+  const dispatch = useDispatch()
+  const isAuth = useIsAuth()
+  const token = localStorage.getItem('token');
+  const [isCheckLoading, setIsCheckLoading] = useState(true)
+  const getIsAuth = async () => {
+    setIsCheckLoading(true)
+    try {
+      await axios('https://partnerev.ru/api/check', {
+        method: 'POST',
+        data: {
+          token
+        }
+      })
+      dispatch(setIsAuthAction(true))
+      setIsCheckLoading(false)
+    } catch (error) {
+      setIsCheckLoading(false)
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      getIsAuth()
+    } else {
+      setIsCheckLoading(false)
+    }
+  }, [])
+
   return (
     <div className={styles.app}>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/:categoryKey/*" element={<CategoryPage />} />
-          <Route path="/:categoryKey/:itemId" element={<ItemPage />} />
-          {/* <Route path="/:item" element={<ItemPage />}></Route> */}
-          <Route path="/ashurovvitaly" element={<AshurovCardPage />}></Route>
-          <Route path="/rasulovelshan" element={<RasulovCardPage />}></Route>
-          <Route
-            path="/derevitskayaevgenia"
-            element={<DerevitskayaCardPage />}
-          ></Route>
-          <Route path="/login" element={<AuthPage />} />
-          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
-        </Routes>
-        <ContactForm />
-        <Footer />
+        {
+          isCheckLoading ? <h3>Загрузка</h3> // TODO: В последствии поставить лоадер
+          : <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<MainPage />}></Route>
+              <Route path="/:categoryKey/*" element={<CategoryPage />} />
+              <Route path="/:categoryKey/:itemId" element={<ItemPage />} />
+              {/* <Route path="/:item" element={<ItemPage />}></Route> */}
+              <Route path="/ashurovvitaly" element={<AshurovCardPage />}></Route>
+              <Route path="/rasulovelshan" element={<RasulovCardPage />}></Route>
+              <Route
+                path="/derevitskayaevgenia"
+                element={<DerevitskayaCardPage />}
+              ></Route>
+             {!isAuth && <Route path="/login" element={<AuthPage />} />}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+            <ContactForm />
+            <Footer />
+          </>
+        }
+        
       </BrowserRouter>
       <ToastContainer autoClose={1500} pauseOnHover={false} />
     </div>
